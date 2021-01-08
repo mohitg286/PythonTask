435e64c0e16f2b6269aa26991eba55ae0c1dbd07
mohitg286 <77051322+mohitg286@users.noreply.github.com>
2021-01-08 18:16:26 +0530

    Add files via upload

---
+++ Casting.py
@@ -0,0 +1,9 @@
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3")
+z = float("3")   # z will be 3.0
+w = float("4.2")
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)
