b0b20f032756218ff2c81b51ddf602e5cc307022
mohitg286 <77051322+mohitg286@users.noreply.github.com>
2021-01-11 18:43:36 +0530

    Add files via upload

---
+++ Looptuples.py
@@ -0,0 +1,10 @@
+"""
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+"""  
+  
+  thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
--- Tupples.py
+++ Tupples.py
@@ -5,7 +5,14 @@ print(len(thistuple))
 
 thistuple = ("apple",)
 print(type(thistuple))
-
+"""
 #NOT a tuple
 thistuple = ("apple")
 print(type(thistuple))
+"""
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+print(thistuple[1])
+print(thistuple[-1])
+print(thistuple[2:])
