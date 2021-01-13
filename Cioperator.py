d17f802a6b6660dc8174eb5658a5286ba0983633
mohitg286 <77051322+mohitg286@users.noreply.github.com>
2021-01-13 18:39:00 +0530

    Add files via upload

---
+++ Cioperator.py
@@ -0,0 +1,8 @@
+x = 5
+y = 3
+
+print(x == y) # returns False because 5 is not equal to 3
+print(x != y) # returns True because 5 is not equal to 3
+print(x > y) # returns true
+print(x >= y)
+print(x <= y)
---
+++ Function.py
@@ -0,0 +1,9 @@
+"""
+def my_function():
+  print("Hello from a Mohit")
+"""  
+ 
+ def my_function():
+  print("Hello from a Mohit")
+
+my_function()
