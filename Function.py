d1aaa63e4720ac62ac3987b95182a981120ebacb
mohitg286 <77051322+mohitg286@users.noreply.github.com>
2021-01-18 18:37:14 +0530

    Add files via upload

--- Function.py
+++ Function.py
@@ -2,8 +2,17 @@
 def my_function():
   print("Hello from a Mohit")
 """  
- 
+ """
  def my_function():
   print("Hello from a Mohit")
 
 my_function()
+"""
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+
+
+
